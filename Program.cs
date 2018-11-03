0cd1d2e29869008553e5e32cf457ef9583e26dd1
pw257 <42645110+pw257@users.noreply.github.com>
2018-11-02 19:00:04 -0500

    Document

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PatrickW_Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Document\n");
+
+            Console.Write("Enter the name of your document: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the content of your document: ");
+            string content = Console.ReadLine();
+
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(name + ".txt");
+                streamWriter.WriteLine(content);
+                streamWriter.Close();
+                Console.WriteLine("{0} was successfully saved. The document contains {1} characters.", name, content.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
+
